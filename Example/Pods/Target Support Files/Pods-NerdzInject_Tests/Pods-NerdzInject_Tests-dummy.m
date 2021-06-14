ca8e497a4bcc930593a0b7c91dc9c23b7de423d7
Vasyl Khmil <vasyl@nerdzlab.com>
2021-06-14 13:36:28 +0300

    - removed unnecessary pods;

--- Example/Pods/Target Support Files/Pods-NerdzInject_Tests/Pods-NerdzInject_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NerdzInject_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_NerdzInject_Tests
-@end
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import NerdzInjectTests
-
-var tests = [XCTestCaseEntry]()
-tests += NerdzInjectTests.allTests()
-XCTMain(tests)
